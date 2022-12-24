492024ee9b6a25381814a7c6335a4c10548809b5
gunel <lothlovien@gmail.com>
2022-12-24 18:47:33 +0400

    исправили ошибки во втором задании

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -4,10 +4,21 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-if (a < b)
+int max = a;
+int min = b;
+
+if (a > b) 
+{  
+    max = a;
+    min = b;
+}
+else
 {
-    Console.WriteLine("max=b");
-}
-else{
-    Console.WriteLine("max=a");
-}
+      max = b;
+      min = a;
+} 
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
